5cba9090a7aa949cf8bcacf2def8464bb669c98e
Iris Membrere <irismembrere@gmail.com>
2023-05-16 23:35:39 +0800

    Assigning values

--- TestTV.py
+++ TestTV.py
@@ -6,10 +6,16 @@ def TestTV():
     tv1 = TV("", "")
     tv2 = TV("", "")    
 
-# Assign the values for TV1
+    # Assign the values for TV1
+    tv1.channel_setting(30)
+    tv1.volume_setting(3)
+    tv1.TV_on()
 
-# Assign the values for TV2
+    # Assign the values for TV2
+    tv2.channel_setting(3)
+    tv2.volume_setting(2)
+    tv2.TV_on()
 
-# Print the values
+    # Print the values
 
 # End the program
