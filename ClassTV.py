40c0a53ce73b20b727cdfc38731b06861c04eacf
Iris Membrere <irismembrere@gmail.com>
2023-05-16 21:14:29 +0800

    Creating a class for TV and initializing the channel and volume

--- ClassTV.py
+++ ClassTV.py
@@ -1,27 +1,29 @@
 # Create a class named tv
-
-# Create a constructor
-
-# Initialize instance variables
+class TV:
+    # Create a constructor
+    def __init__(self, channel, volume):
+        # Initialize instance variables
+        self.channel = channel
+        self.volume = volume
 
 # Create an instance method for:
-    
-# Turned ON TV
-    
-# Turned OFF TV
-    
-# Getting the channel
-    
-# Setting the channel
-    
-# Getting the volume
-    
-# Setting the volume
-    
-# Switching channels up
-    
-# Switching channels down
-    
-# Turning volume up
-    
-# Turning volume down
+        
+    # Turned ON TV
+        
+    # Turned OFF TV
+        
+    # Getting the channel
+        
+    # Setting the channel
+        
+    # Getting the volume
+        
+    # Setting the volume
+        
+    # Switching channels up
+        
+    # Switching channels down
+        
+    # Turning volume up
+        
+    # Turning volume down
