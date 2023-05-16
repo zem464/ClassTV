2f2e2aa4c1948038d183061c2e9201dea9239751
Iris Membrere <irismembrere@gmail.com>
2023-05-16 20:24:16 +0800

    Pseudocode for Class TV

---
+++ ClassTV.py
@@ -0,0 +1,27 @@
+# Create a class named tv
+
+# Create a constructor
+
+# Initialize instance variables
+
+# Create an instance method for:
+    
+# Turned ON TV
+    
+# Turned OFF TV
+    
+# Getting the channel
+    
+# Setting the channel
+    
+# Getting the volume
+    
+# Setting the volume
+    
+# Switching channels up
+    
+# Switching channels down
+    
+# Turning volume up
+    
+# Turning volume down
